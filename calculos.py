ebe799a56493b56df93c99aed8b55380b2febea9
RodrigoParodi <parodirodrigo0@gmail.com>
2024-05-23 13:43:11 -0300

    Calculadora

--- calculos.py
+++ calculos.py
@@ -1,13 +1,49 @@
 def sumar_enteros(x:int,y:int)->int:
+    """Sumar dos numeros enteros
+
+    Args:
+        x (int): Primer numero
+        y (int): Segundo Numero
+
+    Returns:
+        int: Retorna el resulado de la suma
+    """
     return x + y
 
 def restar_enteros(x:int,y:int)->int:
+    """Resta dos numeros enteros
+
+    Args:
+        x (int): Primer numero
+        y (int): Segundo Numero
+
+    Returns:
+        int: Retorna el resultado de la resta
+    """
     return x - y
 
 def multiplicar_enteros(x:int,y:int)->int:
+    """Multiplcia dos numeros enteros
+
+    Args:
+        x (int): Primer numero
+        y (int): Segundo Numero
+
+    Returns:
+        int: Retorna el resultado de la multiplicacion
+    """
     return x * y
 
 def dividir(x:int, y:int)->int:
+    """Divide dos numeros enteros
+
+    Args:
+        x (int): Primer numero
+        y (int): Segundo Numero
+
+    Returns:
+        int: Retorna el resultado de la operacion y si se dividio por cero retornara 0
+    """
     try:
         resultado = x / y
         return resultado
@@ -16,6 +52,14 @@ def dividir(x:int, y:int)->int:
         return resultado
 
 def factorial(n:int)->int:
+    """Calcula el factorial de un numero
+
+    Args:
+        n (int): Numero a calcular
+
+    Returns:
+        int: Retorna el resultado del factoreo
+    """
     fact = None
     if n == 0 or n == 1:
         fact = 1
--- funMenus.py
+++ funMenus.py
@@ -2,6 +2,14 @@
 from calculos import *
 
 def ingrese_numero_entero(mensaje:str)->int:
+    """Pide el ingreso de un numero entero y valida que lo ingresado sea un numero entero
+
+    Args:
+        mensaje (str): mensaje de lo que queremos pedir
+
+    Returns:
+        int: Retorna el numero entero validado y convertido a entero
+    """
     from os import system
     system("cls")
 
@@ -15,12 +23,26 @@ def ingrese_numero_entero(mensaje:str)->int:
     return numero
 
 def mostrar_mensajes(mensaje):
+    """Limpia la consola ,muestra un mensaje y pausea la consola
+
+    Args:
+        mensaje (_type_): mensaje que queremos mostrar
+    """
     from os import system
     system("cls")
     print(mensaje)
     system("pause")
 
 def menuOpciones(x:int , y:int)->str:
+    """Muestra el Menu principal de la calculadora y pide elejor una opcion
+
+    Args:
+        x (int): Valor X de la calculadora
+        y (int): Valor y de la calculadora
+
+    Returns:
+        str: Retorna la opcion elejida por el usuario
+    """
     from os import system
     system("cls")
     print("----MENU DE OPCIONES---\n")
@@ -34,6 +56,15 @@ def menuOpciones(x:int , y:int)->str:
     return opcion_usuario
 
 def menuCalcular(x:int,y:int):
+    """Sub menu para elejir una operacion
+
+    Args:
+        x (int): Valor X de la calculadora elejido por el usuario
+        y (int): Valor Y de la calculadora elejido por el usuario
+
+    Returns:
+        _type_: Retorna la opcion elejida por el usuario
+    """
     from os import system
     system("cls")
     print(f"A)Calcular la suma ({x}+{y})")
@@ -47,6 +78,17 @@ def menuCalcular(x:int,y:int):
     return opcion_usuario
 
 def seleccionar_operacion(x,y,resultado):
+    """Funcion que se encarga de llamar a las funciones que requiere utilizar el usuario para
+        realizar una operacion y retorna un resultado
+
+    Args:
+        x (int): Valor X de la calculadora elejido por el usuario
+        y (int): Valor Y de la calculadora elejido por el usuario
+        resultado (_type_): resultado inicial
+
+    Returns:
+        _type_: Retorna el resultado de la operacion realizada por el usuario
+    """
     from os import system
 
     while True:
@@ -86,6 +128,17 @@ def seleccionar_operacion(x,y,resultado):
     return mensaje_resultado
 
 def mensaje_division(resultado,x:int,y:int)->str:
+    """Si se dividio por cero mostrara un mensaje de error.
+        Si la division se pudo realizar mostrara su resultado
+
+    Args:
+        resultado (_type_): resultado de la division
+        x (int): Valor X de la calculadora elejido por el usuario
+        y (int): Valor Y de la calculadora elejido por el usuario
+
+    Returns:
+        str: Mensaje que mostrara el resultado o el error
+    """
     if resultado != 0:
         mensaje_resultado = f"El resultado de {x} / {y} es : {resultado}"
     else:
